d6415723f8d6b9e5575b453585646dc07ff5e5f3
TimLeiber <tim.leiber@uni-potsdam.de>
2025-08-06 13:30:27 +0200

    add file for creating instance files based on temporal graphs

---
+++ src/symbolic_module.py
@@ -0,0 +1,19 @@
+import datasets
+import json
+
+
+
+def tg_to_asp(TG) -> str:
+    """
+    transforms a temporal graph to a string which can be written as content into an ASP instance file
+    
+    Args:
+        TG (list) a temporal graph object encoding some story
+    
+    Return:
+        A string containing valid ASP code to be written into an instance file
+    """
+    pass
+
+def create_asp_instance_files(dataset) -> None:
+    pass
